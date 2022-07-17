1551f733170f0558b3d35390defb4f236ba085cb
liad.levy <liadlevy@LiadsMacBook.local>
2022-07-17 13:05:46 +0300

    change endpoint to v1 and v2 for ctf play and cleanup

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -4,8 +4,8 @@ from app.api.routes import authentication, comments, profiles, tags, users, auth
 from app.api.routes.articles import api as articles
 
 router = APIRouter()
-router.include_router(authentication.router, tags=["authentication"], prefix="/users")
-router.include_router(authentication_old.router, tags=["authentication"], prefix="/testing/users")
+router.include_router(authentication.router, tags=["authentication"], prefix="/v2/users")
+router.include_router(authentication_old.router, tags=["authentication"], prefix="/v1/users")
 router.include_router(users.router, tags=["users"], prefix="/user")
 router.include_router(profiles.router, tags=["profiles"], prefix="/profiles")
 router.include_router(articles.router, tags=["articles"])
