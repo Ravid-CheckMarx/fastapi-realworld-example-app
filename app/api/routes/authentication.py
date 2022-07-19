3365a0f819ff1a5f579670a8444c2ac69eb63007
liad.levy <liadlevy@LiadsMacBook.local>
2022-07-19 20:31:38 +0300

    delete register from this file and move it to a new file

--- app/api/routes/authentication.py
+++ app/api/routes/authentication.py
@@ -7,14 +7,12 @@ from app.core.settings.app import AppSettings
 from app.db.errors import EntityDoesNotExist
 from app.db.repositories.users import UsersRepository
 from app.models.schemas.users import (
-    UserInCreate,
     UserInLogin,
     UserInResponse,
     UserWithToken,
 )
 from app.resources import strings
 from app.services import jwt
-from app.services.authentication import check_email_is_taken, check_username_is_taken
 
 router = APIRouter()
 
@@ -65,42 +63,3 @@ async def login(
             ),
         )
 
-
-@router.post(
-    "",
-    status_code=HTTP_201_CREATED,
-    response_model=UserInResponse,
-    name="auth:register",
-)
-async def register(
-    user_create: UserInCreate = Body(..., embed=True, alias="user"),
-    users_repo: UsersRepository = Depends(get_repository(UsersRepository)),
-    settings: AppSettings = Depends(get_app_settings),
-) -> UserInResponse:
-    if await check_username_is_taken(users_repo, user_create.username):
-        raise HTTPException(
-            status_code=HTTP_400_BAD_REQUEST,
-            detail=strings.USERNAME_TAKEN,
-        )
-
-    if await check_email_is_taken(users_repo, user_create.email):
-        raise HTTPException(
-            status_code=HTTP_400_BAD_REQUEST,
-            detail=strings.EMAIL_TAKEN,
-        )
-
-    user = await users_repo.create_user(**user_create.dict())
-
-    token = jwt.create_access_token_for_user(
-        user,
-        str(settings.secret_key.get_secret_value()),
-    )
-    return UserInResponse(
-        user=UserWithToken(
-            username=user.username,
-            email=user.email,
-            bio=user.bio,
-            image=user.image,
-            token=token,
-        ),
-    )
