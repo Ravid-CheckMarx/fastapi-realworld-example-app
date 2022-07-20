04549031c7507c611e8f3a04a51d91f9c35bd5b7
liad.levy <liadlevy@LiadsMacBook.local>
2022-07-20 15:12:26 +0300

    add membership route

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.routes import authentication, comments, profiles, tags, users, admin, authentication_old, debug, register
+from app.api.routes import authentication, comments, profiles, tags, users, admin, authentication_old, debug, register,\
+    membership
 from app.api.routes.articles import api as articles
 
 router = APIRouter()
@@ -8,6 +9,7 @@ router.include_router(register.router, tags=["authentication"], prefix="/users")
 router.include_router(authentication.router, tags=["authentication"], prefix="/v2/users")
 router.include_router(authentication_old.router, tags=["authentication"], prefix="/v1/users")
 router.include_router(users.router, tags=["users"], prefix="/user")
+router.include_router(membership.router, tags=["membership"], prefix="/membership")
 router.include_router(admin.router, tags=["admin"], prefix="/admin")
 router.include_router(profiles.router, tags=["profiles"], prefix="/profiles")
 router.include_router(articles.router, tags=["articles"])
