2cd0841ffb4440f1816db26493cfdcb90ac91a8c
liad.levy <liadlevy@LiadsMacBook.local>
2022-07-21 14:12:09 +0300

    clean code

--- app/api/routes/membership.py
+++ app/api/routes/membership.py
@@ -1,17 +1,10 @@
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from starlette.status import HTTP_400_BAD_REQUEST
 
 from app.api.dependencies.authentication import get_current_user_authorizer
-from app.api.dependencies.database import get_repository
-from app.core.config import get_app_settings
-from app.core.settings.app import AppSettings
-from app.db.repositories.users import UsersRepository
+
 from app.models.domain.users import User
-from app.models.schemas.users import UserInResponse, UserInUpdate, UserWithToken
-from app.resources import strings
-from app.services import jwt
-from app.services.authentication import check_email_is_taken, check_username_is_taken
+from app.resources.strings import ExcessiveDataExposure, DescriptionExcessiveDataExposure
 
 router = APIRouter()
 
@@ -23,19 +16,18 @@ class Member(BaseModel):
     name: str
 
 
-@router.post("")
-async def member(
+@router.post("", name="Membership:Subscribe")
+async def membership(
         member: Member,
         user: User = Depends(get_current_user_authorizer()),
 
 ):
-    print(member)
     if (
-    member.number == '4426111122223333' and
-    member.cvc == '555' and
-    member.name == 'Team Rocket' and
-    member.expiry == '0922'
+            member.number == '4426111122223333' and
+            member.cvc == '555' and
+            member.name == 'Team Rocket' and
+            member.expiry == '0922'
     ):
-        return "flag{2_much_1nf0_Xp0seD}"
+        return "{}\n\n{}".format(ExcessiveDataExposure, DescriptionExcessiveDataExposure)
     else:
         return "Card declined, try again!"
