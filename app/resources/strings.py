c09c602586c4f681418da2509e56a3b4c010f07b
liad.levy <liadlevy@LiadsMacBook.local>
2022-07-21 19:04:07 +0300

    reformat flag

--- app/resources/strings.py
+++ app/resources/strings.py
@@ -28,7 +28,7 @@ AUTHENTICATION_REQUIRED = "authentication required"
 
 BrokenFunctionLevelAuthorization = "flag{I_aM_Th3_aDm1n_H3r3!}"
 BrokenUserAuthentication = "flag{F33l_My_V01t_Tack13!}"
-BOLA = "flag{1_0Wn_A11_Res0urc3S!}"
+BOLA = "flag{B0lA!!!!!}"
 ImproperAssetsManagement = "flag{Impr0peR_Ass3ts_ManAg3m3nt}"
 Injection = "flag{1nject10n_Ap1}"
 ExcessiveDataExposure = "flag{3xc3ss1v3_daTa_Xp0sur3}"
